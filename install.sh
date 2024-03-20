54815c71b20971309e5f8aa31111b50a62e298cf
elvxk <pradiptaramadan9c@gmail.com>
2024-03-20 18:55:29 +0700

    fix:oh-my-zsh custom plugins

--- install.sh
+++ install.sh
@@ -27,6 +27,12 @@ brew install git-credential-oauth node oven-sh/bun/bun stow lazygit ripgrep fd x
 
 echo -e "${BLUE}$line${RESET}"
 
+git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
+
+echo -e "${BLUE}$line${RESET}"
+
 git credential-oauth configure
 cd ~/.dotfiles/ && stow .
 
